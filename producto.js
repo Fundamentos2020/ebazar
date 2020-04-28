623fdd99cc3fa378f1304b67340332459d683ded
Jose Luis <joseluisrojasaranda@gmail.com>
2020-04-28 17:33:25 -0500

    Producto mediante JSON

--- producto.js
+++ producto.js
@@ -1,4 +1,66 @@
 
+function cargar() {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "producto.json", true);
+
+    xhr.onload = function() {
+        if(this.status === 200) {
+            let producto = JSON.parse(this.responseText);
+            console.log(producto);
+
+            // Carga los valores del producto
+            let titulo = document.getElementById("p_titulo");
+            titulo.innerText = producto.titulo;
+
+            let precio = document.getElementById("p_precio");
+            precio.innerText = `$ ${producto.precio}`;
+
+            let disponibles = document.getElementById("p_disponibles");
+            disponibles.innerText = `${producto.disponibles} disponibles`;
+
+            let ubicacion = document.getElementById("p_ubicacion");
+            ubicacion.innerText = `Ubicacion: ${producto.ubicacion}`;
+
+            let descripcion_c = document.getElementById("p_descripcion_c");
+            descripcion_c.innerText = `${producto.descripcion_corta}`;
+            
+            let descripcion_l = document.getElementById("p_descripcion_l");
+            descripcion_l.innerText = `${producto.descripcion_larga}`;
+
+            let caracterisitcas = document.getElementById("p_caracteristicas");
+            let c = "";
+            for(let k in producto.caracteristicas) {
+                c += `
+                    <div class="producto_texto producto_caracteristica">
+                        ${k}: ${producto.caracteristicas[k]}
+                    </div>
+                `;
+            }
+            caracterisitcas.innerHTML = c;
+
+            let preguntas = document.getElementById("p_preguntas");
+            let p = "";
+            producto.preguntas.forEach(pregunta => {
+                p += `
+                    <div class="comment_box">
+                        <div class="comment_titulo">
+                            ${pregunta.pregunta}
+                            <div class="comment_fecha">${pregunta.fecha}</div>
+                        </div>
+                        <div class="comment_texto">
+                            ${pregunta.respuesta}
+                        </div>
+                    </div>
+                `;
+            });
+
+            preguntas.innerHTML = p;
+        }
+    }
+
+    xhr.send();
+}
 
 function ClickImgsProducto(e) {
     var imgProducto = document.getElementById("imagen_producto");
